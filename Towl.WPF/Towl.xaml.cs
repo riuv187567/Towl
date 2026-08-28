219e0891bcbadf5c99a0a924f04259c901515dfa
riuv187567 <riuv187567@gmail.com>
2026-08-28 15:11:07 +0300

    Added WPF project

---
+++ Towl.WPF/Towl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Towl.WPF
+{
+    public partial class TowlWindow : Window
+    {
+        public TowlWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
