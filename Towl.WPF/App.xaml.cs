b93fbe5af44a21a6ae3e7f6912cec0dd332fa035
riuv187567 <riuv187567@gmail.com>
2026-08-31 14:55:07 +0300

    Test setup

--- Towl.WPF/App.xaml.cs
+++ Towl.WPF/App.xaml.cs
@@ -1,12 +1,60 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Towl.Core;
+using Towl.Core.Data;
+using Towl.Core.Services;
 
 namespace Towl.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private IHost? _host;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var state = new TowlState()
+            {
+                Data = TowlDataManager.LoadData(),
+                Settings = TowlDataManager.LoadSettings(),
+                CursorMoved = false,
+            };
+
+            var builder = Host.CreateApplicationBuilder();
+
+            builder.Services.AddSingleton(state);
+
+            builder.Services.AddSingleton<DiscordIntegration>();
+            builder.Services.AddHostedService<TimerBackgroundService>();
+
+            static System.Drawing.Point mousePosition()
+            {
+                var point = Mouse.GetPosition(Application.Current.MainWindow);
+                return new System.Drawing.Point((int)point.X, (int)point.Y);
+            }
+
+            builder.Services.AddHostedService(sp => new CursorMovedTestBackgroundService(mousePosition, state));
+
+            builder.Services.AddSingleton<TowlWindow>();
+
+            _host = builder.Build();
+            _host.Start();
+
+            var form = _host.Services.GetRequiredService<TowlWindow>();
+            form.Show();
+
+            _host.StopAsync().GetAwaiter().GetResult();
+            _host.Dispose();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host!.Dispose();
+            base.OnExit(e);
+        }
+    }
 }
--- Towl.WPF/Towl.xaml.cs
+++ Towl.WPF/Towl.xaml.cs
@@ -1,12 +1,59 @@
 ﻿using System.Windows;
+using Towl.Core;
+using Towl.Core.Data;
+using Towl.Core.Utils;
 
 namespace Towl.WPF
 {
     public partial class TowlWindow : Window
     {
-        public TowlWindow()
+        private readonly TowlState _state;
+        private readonly DiscordIntegration _discord;
+
+        public TowlWindow(TowlState state, DiscordIntegration discord)
         {
             InitializeComponent();
+
+            _state = state;
+            _discord = discord;
+
+            Loaded += async (a, b) => RunTimerAsync();
+        }
+
+        private async Task RunTimerAsync()
+        {
+            await UpdateTimer();
+            var time = new PeriodicTimer(TimeSpan.FromSeconds(Constants.CycleSeconds));
+
+            while (await time.WaitForNextTickAsync())
+            {
+                await UpdateTimer();
+                await Task.Delay(Constants.CycleSeconds);
+                TowlDataManager.SaveData(_state.Data);
+            }
+        }
+
+        private async Task UpdateTimer()
+        {
+            var displayedProcName = _state.Settings.DisplayedProcessName;
+            if (!_state.Data.ProcessEntries.TryGetValue(_state.Settings.DisplayedProcessName, out var process))
+            {
+                // sessionTime.BackColor = Constants.NotActiveColor;
+                MainTimeText.Text = Constants.NoProcessDisplayedText;
+            }
+            else
+            {
+                /*
+                if (ProcessUtils.ProcessIsFocused(displayedProcName) && _state.CursorMoved)
+                    MainTimeText.Background = Constants.ActiveColor;
+                else
+                    sessionTime.BackColor = Constants.NotActiveColor;
+                */
+
+                var todayTimeString = TimeUtils.HumanizeTime(_state.Data.GetTodaySeconds(process.Name));
+                MainTimeText.Text = todayTimeString;
+                _discord.SetDescription($"Tracked Time - {todayTimeString}");
+            }
         }
     }
 }
